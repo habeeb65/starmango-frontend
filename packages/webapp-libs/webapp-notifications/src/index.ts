146605ef0b10d260cbb4039e5361d36001772c6a
MOHAMMED HABEEB <117671296+habeeb65@users.noreply.github.com>
2025-05-06 15:28:42 +0530

    Add notification module exports

---
+++ packages/webapp-libs/webapp-notifications/src/index.ts
@@ -0,0 +1,10 @@
+// Export types
+export * from './types';
+
+// Export hooks
+export * from './hooks/useNotifications';
+
+// Export components
+export * from './components/NotificationBell';
+export * from './components/NotificationsList';
+export * from './components/NotificationsProvider';
