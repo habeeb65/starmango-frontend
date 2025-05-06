0c82864966b72f785d1c26735ddee319f80158a1
MOHAMMED HABEEB <117671296+habeeb65@users.noreply.github.com>
2025-05-06 15:30:51 +0530

    Add users module exports

---
+++ packages/webapp-libs/webapp-users/src/index.ts
@@ -0,0 +1,11 @@
+// Export types
+export * from './types';
+
+// Export hooks
+export * from './hooks/useUser';
+export * from './hooks/useProfile';
+export * from './hooks/useUserAvatar';
+
+// Export components
+export * from './components/userAvatar/userAvatar.component';
+export * from './components/profileForm/profileForm.component';
