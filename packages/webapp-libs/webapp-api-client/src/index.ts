2fab9aeeec77d9ddff94ea314711473407f62037
MOHAMMED HABEEB <117671296+habeeb65@users.noreply.github.com>
2025-05-06 15:21:21 +0530

    Add API client module exports

---
+++ packages/webapp-libs/webapp-api-client/src/index.ts
@@ -0,0 +1,12 @@
+// Core API client
+export * from './api/client';
+export * from './api/helpers';
+
+// Authentication
+export * from './api/auth';
+export * from './api/auth/types';
+export * from './api/auth/hooks';
+
+// GraphQL
+export * from './graphql/apolloClient';
+export * from './graphql/fragments';
