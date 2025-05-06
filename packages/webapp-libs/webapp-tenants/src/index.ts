8ad4bf7b11f680416c6ffa5ff35ddf59a6586c01
MOHAMMED HABEEB <117671296+habeeb65@users.noreply.github.com>
2025-05-06 15:26:38 +0530

    Add tenant module exports

---
+++ packages/webapp-libs/webapp-tenants/src/index.ts
@@ -0,0 +1,9 @@
+// Export types
+export * from './types';
+
+// Export hooks
+export * from './hooks/useTenant';
+export * from './hooks/useTenantList';
+
+// Export context
+export * from './tenantContext';
